2cd920e3846125dfea0f0f1ff193ac0a2245b64b
Fedor Melnikov <transzendenz@mail.ru>
2020-04-24 14:09:13 +0300

    end lesson4

---
+++ lesson4/les4.py
@@ -0,0 +1,4 @@
+from itertools import combinations, combinations_with_replacement,  permutations, product
+
+progr_lang = ["python", "java", "perl", "javascript"]
+print(list(product(progr_lang, repeat=3)))
